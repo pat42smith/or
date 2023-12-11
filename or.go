8e8dfda34561f194a302174537e6657c3bbac90e
Patrick Smith <pat42smith@gmail.com>
2023-12-10 18:00:46 -0800

    Comment on omitted Returnn functions.

--- or.go
+++ or.go
@@ -37,4 +37,10 @@
 // [this proposal](https://github.com/golang/go/issues/64197#issuecomment-1814245087).
 // If that proposal is implemented, then the functions here may be changed to
 // the preferred order.
+//
+// It is technically possible to add a set of Returnn functions that would simulate
+// a return from the containing function to return when the error is not nil.
+// But this would use panic(), and seems to be a misuse. Also, it would require
+// explicit deferred code in the containing function. For these reasons, the Returnn
+// functions have been omitted.
 package or
